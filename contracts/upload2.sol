dc24f6ee8aab935aea1738efab654a9112d06327
Ankit Kishan <91215261+ankit081202@users.noreply.github.com>
2023-05-22 16:53:19 +0530

    Add files via upload

---
+++ contracts/upload2.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract upload2{
+    struct Memo {
+        uint256 timestamp;
+        string items;
+    }
+
+    Memo[] memos;
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buy(string memory items) public payable {
+        require(msg.value > 0, "Please pay greater than 0 ether");
+        owner.transfer(msg.value);
+        memos.push(Memo(block.timestamp,items));
+    }
+
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+}
